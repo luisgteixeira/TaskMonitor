8a96c375e703d9f99f9c5ad61467d5fa9070dcb2
Luis Guilherme <luisguilherme.ufpi@gmail.com>
2016-03-29 11:58:57 -0300

    Emails sao repetidos de acordo com o tempo pre-estabelecido. Falta arrumar a condicao de parada do envio.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time
 from sendmail import *
 
 def main():
+
+    running = 3
 
     from_mail = 'logexecution@gmail.com'
     password = 'nossasenhaeessa'
@@ -11,10 +14,19 @@ def main():
     #info_execution = 'execucao 1'
     info_execution = ''
 
-    sendmail = SendMail(from_mail, password, to, info_execution)
+    # Tempo de execucao em minutos
+    execution_time = 1
 
-    # Envia email
-    sendmail.send()
+    while running > 0:
+        # Cria email a ser enviado
+        sendmail = SendMail(from_mail, password, to, info_execution)
+
+        # Envia email
+        sendmail.send()
+
+        # Tempo entre o envio dos emails
+        time.sleep(execution_time * 60)
+        running -= 1
 
 if __name__ == '__main__':
     main()
