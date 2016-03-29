dc46bc369d49a35eea4d6615fc3b96bd9cf0546a
Neto Araujo <netoaraujjo@gmail.com>
2016-03-29 11:12:35 -0300

    Criada classe logmonitor

---
+++ logmonitor.py
@@ -0,0 +1,9 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+
+class LogMonitor(object):
+    """docstring for LogMonitor"""
+    def __init__(self, arg):
+        super(LogMonitor, self).__init__()
+        self.arg = arg
