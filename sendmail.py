078086fe08822d9bcef340dc7cc410f0c902faa9
Luis Guilherme <luisguilherme.ufpi@gmail.com>
2016-03-29 16:01:54 -0300

    Melhoria no arquivo de configuracao. Informacoes sao lidas do arquivo e passadas a classe que envia o email. Envia email com informacao de execucao no texto.

--- sendmail.py
+++ sendmail.py
@@ -8,15 +8,18 @@ import smtplib
 
 class SendMail(object):
 
-    def __init__(self, mail_from, password, mail_to, execution_info):
+    def __init__(self, mail_from, password, mail_to, execution_info, execution_info_largest):
         self.mail_from = mail_from
         self.password = password
-        self.mail_to = mail_to
+        self.mail_to = mail_to.split(',')
+
         # Existe algo na string
         if execution_info:
             self.execution_info = ' (' + execution_info + ')'
         else:
             self.execution_info = execution_info
+
+        self.execution_info_largest = execution_info_largest
 
     def send(self):
         smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
@@ -25,9 +28,7 @@ class SendMail(object):
 
         self.mail_from = 'Informação de Log<' + self.mail_from + '>'
 
-        self.msg = """From: %s\nTo: %s\nSubject: Log de Execucao%s
-
-        Email de teste do Log de Execucao.""" % (self.mail_from, ', '.join(self.mail_to), self.execution_info)
+        self.msg = """From: %s\nTo: %s\nSubject: Log de Execucao%s\n%s\n\nEmail de teste do Log de Execucao.""" % (self.mail_from, ', '.join(self.mail_to), self.execution_info, self.execution_info_largest)
 
         smtp.sendmail(self.mail_from, self.mail_to, self.msg)
 
