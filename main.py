08d81ee13d04c9634a533214f49cdc43e5425af1
Luis Guilherme <luisguilherme.ufpi@gmail.com>
2016-03-29 15:04:19 -0300

    Modificacao no nome de variaveis.

--- main.py
+++ main.py
@@ -8,24 +8,26 @@ def main():
 
     running = 3
 
-    from_mail = 'logexecution@gmail.com'
+    '''
+    mail_from = 'logexecution@gmail.com'
     password = 'nossasenhaeessa'
-    to = 'luisguilherme.ufpi@gmail.com'
-    #info_execution = 'execucao 1'
-    info_execution = ''
+    mail_to = 'luisguilherme.ufpi@gmail.com'
+    #execution_info = 'execucao 1'
+    execution_info = ''
 
     # Tempo de execucao em minutos
-    execution_time = 1
+    notification_time = 1
+    '''
 
     while running > 0:
         # Cria email a ser enviado
-        sendmail = SendMail(from_mail, password, to, info_execution)
+        sendmail = SendMail(mail_from, password, mail_to, execution_info)
 
         # Envia email
         sendmail.send()
 
         # Tempo entre o envio dos emails
-        time.sleep(execution_time * 60)
+        time.sleep(notification_time * 60)
         running -= 1
 
 if __name__ == '__main__':
--- sendmail.py
+++ sendmail.py
@@ -8,27 +8,27 @@ import smtplib
 
 class SendMail(object):
 
-    def __init__(self, from_mail, password, to, info_execution):
-        self.from_mail = from_mail
+    def __init__(self, mail_from, password, mail_to, execution_info):
+        self.mail_from = mail_from
         self.password = password
-        self.to = [to]
+        self.mail_to = mail_to
         # Existe algo na string
-        if info_execution:
-            self.info_execution = ' (' + info_execution + ')'
+        if execution_info:
+            self.execution_info = ' (' + execution_info + ')'
         else:
-            self.info_execution = info_execution
+            self.execution_info = execution_info
 
     def send(self):
         smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 
-        smtp.login(self.from_mail, self.password)
+        smtp.login(self.mail_from, self.password)
 
-        self.from_mail = 'Informação de Log<' + self.from_mail + '>'
+        self.mail_from = 'Informação de Log<' + self.mail_from + '>'
 
         self.msg = """From: %s\nTo: %s\nSubject: Log de Execucao%s
 
-        Email de teste do Log de Execucao.""" % (self.from_mail, ', '.join(self.to), self.info_execution)
+        Email de teste do Log de Execucao.""" % (self.mail_from, ', '.join(self.mail_to), self.execution_info)
 
-        smtp.sendmail(self.from_mail, self.to, self.msg)
+        smtp.sendmail(self.mail_from, self.mail_to, self.msg)
 
         smtp.quit()
